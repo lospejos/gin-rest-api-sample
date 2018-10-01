7b2e89937f11a99a0180fce1c0d0e28b170c22ff
lospejos <lospejos@gmail.com>
2018-10-01 13:21:52 +0400

    Changed DB from MySQL to PostgreSQL in database.go and .env files

--- database/database.go
+++ database/database.go
@@ -5,14 +5,16 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql" // configures mysql driver
+	//_ "github.com/jinzhu/gorm/dialects/mysql" // configures mysql driver
+	_ "github.com/jinzhu/gorm/dialects/postgres" // configures postgres driver
 	"github.com/velopert/gin-rest-api-sample/database/models"
 )
 
 // Initialize initializes the database
 func Initialize() (*gorm.DB, error) {
 	dbConfig := os.Getenv("DB_CONFIG")
-	db, err := gorm.Open("mysql", dbConfig)
+	//db, err := gorm.Open("mysql", dbConfig)
+	db, err := gorm.Open("postgres", dbConfig)
 	db.LogMode(true) // logs SQL
 	if err != nil {
 		panic(err)
